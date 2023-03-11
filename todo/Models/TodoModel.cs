3c6d63311cacee3ba54b1e14eab977b2cf14b8ff
Emanuel Jeisom <emjeisom.dev@gmail.com>
2023-03-11 17:34:20 -0300

    Todo Project: created model TodoModel

---
+++ todo/Models/TodoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace todo.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
