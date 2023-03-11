b94273cb0eb6b16cd096c94f34fb5ddcf1e57a3f
Emanuel Jeisom <emjeisom.dev@gmail.com>
2023-03-11 18:19:39 -0300

    Todo Project: created DataContext

---
+++ todo/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo.Models;
+
+namespace todo.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<TodoModel> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=localhost,1433;
+                                    Database=todo;
+                                    User ID=sa;
+                                    Password=1q2w3e4r@#$;
+                                    Encrypt=false;
+                                    TrustServerCertificate=True;
+                                    MultiSubnetFailover=True;");
+
+    }
+}
