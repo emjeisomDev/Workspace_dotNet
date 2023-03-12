ffa7cc86581644a22ed6eb8050d187f1777ecc36
Emanuel Jeisom <emjeisom.dev@gmail.com>
2023-03-12 13:01:13 -0300

    Todo Project: created HomeController

---
+++ todo/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace todo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet("/")]
+        public string Get()
+        {
+            return "Hello world! Congrats, the controller is working.";
+        }
+
+    }
+}
