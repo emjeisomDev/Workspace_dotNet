b818970f04b3a9e3ed86413147b6c6a215489cbd
emjeisomDev <emjeisom.dev@gmail.com>
2023-07-10 10:34:43 -0300

    Created calculator project

---
+++ EFCore.WebAPI/EFCore.WebAPI/Data/HeroContext.cs
@@ -0,0 +1,21 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroContext : DbContext
+    {
+        public DbSet<Hero> Heros { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=DESKTOP-MN8G6NB\\SQLEXPRESS,1433;
+                                        Database=HeroApp;
+                                        User Id=sa;
+                                        Password=1q2w3e4r@#$;
+                                        Encrypt=false;
+                                        TrustServerCertificate=True;
+                                        MultiSubnetFailover=True;");
+    }
+}
---
+++ EFCore.WebAPI/EFCore.WebAPI/Models/Hero.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore.WebAPI.Models
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Battle Battle { get; set; }
+        public int BattleId { get; set; }
+    }
+}
